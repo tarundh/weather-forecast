a102e35fea8e10bfccca35b1f93618afa861979a
tarun dahiya <tarundh1997@gmail.com>
2022-10-17 18:10:37 +0530

    made some changes in app.js and index.html. Also added body-parser

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
 const https = require("https");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html");
+});
 
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=21dd93ec0cdcd8c9cdc0f2aad0ebf425&units=metric"
+app.post("/", function(req, res){
+    const query = req.body.cityName;
+    const apiKey = "21dd93ec0cdcd8c9cdc0f2aad0ebf425";
+    const unit = "metric"
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey +"&units=" + unit;
+    
     https.get(url, function(response){
         console.log(response.statusCode);
 
@@ -16,17 +26,15 @@ app.get("/", function(req, res){
             const icon = weatherData.weather[0].icon
             const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
 
-            res.write("<p>The weather is currently " + weather + " </p>")
-            res.write("<h1>The temperature in london is " + temp + " degree celsius</h1>")
+            res.write("<p>The weather is currently " + weather + " </p>");
+            res.write("<h1>The temperature in london is " + temp + " degree celsius</h1>");
             res.write("<img src=" + imageURL + ">");
-
-            
-            res.send();
-        })
-
-    })
+             res.send();
+    });
 
 });
+
+});    
 
 app.listen(3000, function(){
     console.log("server is running on port 3000")
